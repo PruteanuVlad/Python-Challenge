12953b06c1bcc7b210b214263fcb14125681a961
PruteanuVlad <69005128+PruteanuVlad@users.noreply.github.com>
2020-12-09 17:55:02 +0200

    Update Challenge 4.py

--- Challenge 4.py
+++ Challenge 4.py
@@ -1,11 +1,16 @@
 import urllib.request
-nothing=12345
-
+nothing="[8022]"
+i=0
 while 1:
-    url='http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(nothing)[1:-2]
+    url='http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(nothing)[1:-1]
     fhand = urllib.request.urlopen(url)
     for line in fhand:
         print(line.decode().strip(),end=" ")
-        nothing=[int(s) for s in line.decode().strip().split() if s.isdigit()]
-        print(url,end="")
+        print(i,end=" ")
         print(str(nothing)[1:-1])
+        nothing=[int(s) for s in line.decode().strip().split() if s.isdigit()]
+        if str(nothing)[1:-1]=="72758":
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        #print(url,end="")
+        i=i+1
+
